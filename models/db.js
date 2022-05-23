b15f8c040295676178e4c7e17f33e7e03a56c96a
Andomahefa <ando6651493523@gmail.com>
2022-05-23 14:35:47 +0400

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/MobileDB', (err) => {
+mongoose.connect('mongodb+srv://Andomahefa:Andomahefa2001.@cluster0.lne4g.mongodb.net/test', (err) => {
     if (!err) {
         console.log('Connection MongoDB success.');
     } else {
